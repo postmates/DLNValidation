7d2ac573f07c76eab8e4b597ace2f5dede9a3321
Mary Stufflebeam <marystufflebeam@gmail.com>
2015-02-19 11:30:21 -0800

    moved everything back to root dir and put info in py_modules in setup

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='dl-number-validation',
-    version='0.1.0',
+    version='0.1.2',
     author='Mary Stufflebeam',
     author_email='mary@postmates.com',
-    packages=['dl-number-validation'],
+    py_modules=['dl_number_validation'],
     url='http://pypi.python.org/pypi/dl-number-validation/',
     license='LICENSE.txt',
     description='Provides validation of USA drivers license numbers',

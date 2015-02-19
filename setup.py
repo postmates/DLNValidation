939715b2fd2079e1ca844052bd5c61025aedc868
Mary Stufflebeam <marystufflebeam@gmail.com>
2015-02-19 11:48:26 -0800

    rename

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='dl-number-validation',
+    name='DLNValidation',
     version='0.1.2',
     author='Mary Stufflebeam',
     author_email='mary@postmates.com',
     py_modules=['dl_number_validation'],
-    url='http://pypi.python.org/pypi/dl-number-validation/',
+    url='http://pypi.python.org/pypi/DLNValidation/',
     license='LICENSE.txt',
-    description='Provides validation of USA drivers license numbers',
+    description='Provides validation of the format of USA drivers license numbers',
     long_description=open('README.txt').read(),
     install_requires=[],
 )

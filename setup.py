6aa4091cf57f9fcc79ce9d1ac3179ca09f863109
Mary Stufflebeam <marystufflebeam@gmail.com>
2015-02-25 15:25:29 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='DLNValidation',
-    version='0.1.3',
+    version='0.1.4',
     author='Mary Stufflebeam',
     author_email='mary@postmates.com',
     py_modules=['dlnvalidation'],
